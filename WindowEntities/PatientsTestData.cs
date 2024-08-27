42c6859e205bc5389db723a6e3b9fe4ab6f30e23
krasutskaaa <krasutskadiana@gmail.com>
2024-08-27 13:01:16 +0300

    A PatientsTestData class was added to set an object instance for the PatientsTestsDataDataGrid in the PatientsTestsDataStackPanel in the MainWindow

---
+++ WindowEntities/PatientsTestData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace CourseworkMedicalServerDesktopApplication.WindowEntities;
+
+class PatientsTestData
+{
+    public string Number { get; set; }
+    public string Name { get; set; }
+    public double Value { get; set; }
+    public string Measurement { get; set; }
+    public Brush RadioButtonBorderBrush { get; set; }
+    public Brush RadioButtonBackground { get; set; }
+}
