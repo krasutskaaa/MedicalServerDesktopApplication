45c5392bfc847ae871bc64772ffeccae5360da39
krasutskaaa <krasutskadiana@gmail.com>
2024-08-27 12:58:18 +0300

    A PatientsTests was appended to provide an object instance for the PatientsTestsDataGrid in the PatientsTestsStackPanel in the MainWindow

---
+++ WindowEntities/PatientsTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace CourseworkMedicalServerDesktopApplication.WindowEntities;
+
+class PatientsTests
+{
+    public string Number { get; set; }
+    public string Name { get; set; }
+    public string Date { get; set; }
+    public Brush RadioButtonBorderBrush { get; set; }
+    public Brush RadioButtonBackground { get; set; }
+}
