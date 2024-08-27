243adaebc90507eeae18c8710c8a918cf61b8d8e
krasutskaaa <krasutskadiana@gmail.com>
2024-08-27 13:48:12 +0300

    A private Doctor field and an OnCreateAccountClicked event were produced in the MainWindow partial class

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,24 +1,72 @@
-﻿using System.Text;
+﻿using SharedLibrary.Dtos;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using SharedLibrary.Abstractions.Entities;
+using SharedLibrary.Encryption;
+using CourseworkMedicalServerDesktopApplication.DataServices;
+namespace CourseworkMedicalServerDesktopApplication;
 
-namespace CourseworkMedicalServerDesktopApplication
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+/// 
+
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    private Doctor _doctor;
+    public MainWindow()
     {
-        public MainWindow()
+        InitializeComponent();
+    }
+    private async void OnCreateAccountClicked(object sender, RoutedEventArgs e)
+    {
+        var apiMethod = new DataService("http://localhost:5235/");
+        var newDoctor = new CreateDoctorDto
+        (
+            NameTextBox.Text,
+            GenderTextBox.Text,
+            UsernameTextBox2.Text,
+            PasswordTextBox2.Password.GenerateSHA256(),
+            SpecializationTextBox.Text
+        );
+        await apiMethod.AddNewDoctorAsync(newDoctor);
+        _doctor = await apiMethod.GetDoctorByUsernameAsync(UsernameTextBox2.Text);
+        if (_doctor != null)
         {
-            InitializeComponent();
+            Storyboard storyboard = (Storyboard)this.Resources["Storyboard7"];
+            storyboard.Begin();
+            NameTextBox.Clear();
+            GenderTextBox.Clear();
+            UsernameTextBox2.Clear();
+            PasswordTextBox2.Clear();
+            SpecializationTextBox.Clear();
+
+            DoctorsNameLabel.Content = new TextBlock
+            {
+                Inlines =
+            {
+                    new Run("Doctor's name: "){FontWeight = FontWeights.Bold },
+                    new Run(_doctor.Name)
+                }
+            };
+            DoctorsSpecializationLabel.Content = new TextBlock
+            {
+                Inlines =
+            {
+                    new Run("Doctor's specialization: "){FontWeight = FontWeights.Bold },
+                    new Run(_doctor.Specialization)
+                }
+            };
         }
     }
 }
+
